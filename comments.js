2fe438ece99de98c3c2a61d71856256b88660223
PatrickZuby <123277874+PatrickZuby@users.noreply.github.com>
2024-05-26 15:31:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,50 @@
-// Creat web server
-// 1. Load http module
-var http = require('http');
+// Create web server
+// Use 'express' module
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+// Use 'body-parser' module
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// 2. Create server
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+// Use 'express' module to create a web server
+app.use(express.static('public'));
 
-// 3. Log message
-console.log('Server running at http://webserver:8080/');
+// Get comments from file
+app.get('/api/comments', function(req, res) {
+  fs.readFile('comments.json', 'utf8', function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Server Error');
+      return;
+    }
+    res.send(data);
+  });
+});
+
+// Add comment to file
+app.post('/api/comments', function(req, res) {
+  fs.readFile('comments.json', 'utf8', function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Server Error');
+      return;
+    }
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Server Error');
+        return;
+      }
+      res.send('Success');
+    });
+  });
+});
+
+// Start server
+app.listen(3000, function() {
+  console.log('Server is running on port 3000!');
+});
