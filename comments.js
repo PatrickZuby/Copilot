6ae43aa364a8a2537ec894eeceacbc40d458f1bd
PatrickZuby <123277874+PatrickZuby@users.noreply.github.com>
2024-05-26 15:26:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,12 @@
 // Creat web server
-// Start server
+// 1. Load http module
+var http = require('http');
+
+// 2. Create server
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
+// 3. Log message
+console.log('Server running at http://webserver:8080/');
